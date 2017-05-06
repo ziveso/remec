e967fd1e10335d76304fb5203e6902b4f2c236cb
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2017-05-07 01:47:49 +0700

    TCPServer javadoc

--- desktop/src/touchyou/TCPServer.java
+++ desktop/src/touchyou/TCPServer.java
@@ -1,34 +1,36 @@
 package touchyou;
 
-import java.io.IOException;
-
 import com.lloseng.ocsf.server.AbstractServer;
 import com.lloseng.ocsf.server.ConnectionToClient;
 
+/**
+ * A TCP/IP socket server for communication with Android device.
+ * 
+ * @author Kongpon Charanwattanakit
+ *
+ */
 public class TCPServer extends AbstractServer {
 
+    /**
+     * Initialize the server with a given port.
+     * 
+     * @param port
+     *            is the port to listen to
+     */
     public TCPServer(int port) {
 	super(port);
-	// TODO Auto-generated constructor stub
     }
 
     @Override
     protected void clientConnected(ConnectionToClient client) {
 	super.clientConnected(client);
 	System.out.println("Client connected.");
-	// Only 1 remote allowed.
-	this.stopListening();
     }
 
     @Override
     protected synchronized void clientDisconnected(ConnectionToClient client) {
 	super.clientDisconnected(client);
 	System.out.println("Client disconnected.");
-	try {
-	    this.listen();
-	} catch (IOException e) {
-	    e.printStackTrace();
-	}
     }
 
     @Override
