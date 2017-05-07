a65027c0f95cc7a040ca92d32ba472e7597b93a9
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2017-05-07 15:30:16 +0700

    remove unused import

--- desktop/src/touchyou/gui/WidgetPanel.java
+++ desktop/src/touchyou/gui/WidgetPanel.java
@@ -1,19 +1,15 @@
 package touchyou.gui;
 
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
