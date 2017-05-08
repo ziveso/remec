e92f458bbe8e69368c6f8e1e92a8118c2439b435
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2017-05-08 18:16:42 +0700

    mybutton

--- desktop/src/touchyou/App.java
+++ desktop/src/touchyou/App.java
@@ -6,6 +6,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import touchyou.gui.TouchyouGui;
 
 /**
--- desktop/src/touchyou/gui/add/ByButton.java
+++ desktop/src/touchyou/gui/add/ByButton.java
@@ -1,20 +1,18 @@
 package touchyou.gui.add;
 
+import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridLayout;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 import touchyou.App;
 import touchyou.Command;
-import touchyou.gui.MainFrame;
 import touchyou.gui.ModelPanel;
 import touchyou.util.GuiUtil;
+
 /**
  * 
  * @author Thitiwat Thongbor
@@ -22,27 +20,29 @@ import touchyou.util.GuiUtil;
  */
 public class ByButton extends JButton {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 8391021197211750760L;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 8391021197211750760L;
 
-	public ByButton(Dimension size, App app) {
-		super("Button");
-		this.addActionListener(e -> {
-			JButton but = new JButton();
-			MouseListener ml = new MouseMotion();
-			Command com = new Command();
-			com.setCombination("ALT + F4");
-			// TODO set id
-			app.getProfile().addCommand(com);
-			but.addMouseListener(ml);
-			but.addMouseMotionListener((MouseMotionListener) ml);
-			but.setPreferredSize(new Dimension(50, 50));
-			ModelPanel.getMobile().add(but);
-			but.setBounds(GuiUtil.getInitBound(but.getPreferredSize()));
-			ModelPanel.updateComponent();
-		});
-		this.setPreferredSize(size);
-	}
+    public ByButton(Dimension size, App app) {
+	super("Button");
+	this.addActionListener(e -> {
+	    JButton but = new JButton();
+	    MouseListener ml = new MouseMotion();
+	    Command com = new Command();
+	    com.setCombination("ALT + F4");
+	    // TODO set id
+	    app.getProfile().addCommand(com);
+	    but.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	    but.setOpaque(true);
+	    but.addMouseListener(ml);
+	    but.addMouseMotionListener((MouseMotionListener) ml);
+	    but.setPreferredSize(new Dimension(50, 50));
+	    ModelPanel.getMobile().add(but);
+	    but.setBounds(GuiUtil.getInitBound(but.getPreferredSize()));
+	    ModelPanel.updateComponent();
+	});
+	this.setPreferredSize(size);
+    }
 }
