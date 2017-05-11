122ea0d509ecc0c3d2ee03fe04034d9c48e18b13
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2017-05-11 23:48:51 +0700

    finish record key

--- desktop/src/touchyou/gui/SettingPanel.java
+++ desktop/src/touchyou/gui/SettingPanel.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 
 import javax.swing.ButtonGroup;
@@ -90,7 +91,50 @@ public class SettingPanel extends JPanel {
 	combination = new JTextField();
 	combination.setHorizontalAlignment(SwingConstants.TRAILING);
 	combination.setEditable(false);
-	combination.addKeyListener(new RecordKey(combination , Controller.getInstance().getCurrentCommand()));
+	combination.addKeyListener(new KeyListener() {
+	    private StringBuffer string;
+	    private int pressing = 0;
+
+	    @Override
+	    public void keyPressed(KeyEvent e) {
+		if (pressing == 0) {
+		    string = new StringBuffer();
+		}
+		pressing++;
+		switch (e.getKeyCode()) {
+		case 16:
+		    string.append("Shift +");
+		    break;
+		case 17:
+		    string.append("Ctrl +");
+		    break;
+		case 18:
+		    string.append("Alt +");
+		    break;
+		default:
+		    string.append(e.getKeyChar() + " +");
+		}
+		combination.setText(string.toString());
+		
+	    }
+	    
+	    @Override
+	    public void keyReleased(KeyEvent e) {
+		pressing--;
+		if (pressing == 0) {
+		    string.deleteCharAt(string.length() - 1);
+		    Controller.getInstance().getCurrentCommand().setCombination(string.toString());
+		    Controller.getInstance().updateCurrentCommand();
+		    string = null;
+		}
+	    }
+
+	    @Override
+	    public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	    }
+	});
 	GridBagConstraints gbc_combination = new GridBagConstraints();
 	gbc_combination.gridwidth = 2;
 	gbc_combination.insets = new Insets(0, 0, 5, 0);
